b417c6dce5e992b43315407aafb2b81d9a9bd335
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-03-10 21:49:47 -0300

    Add files via upload

---
+++ Ex. 1002 URI (Área do Círculo)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, n, R;
+
+            n = 3.14159;
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            area = n * R * R;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
