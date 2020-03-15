c19da15f8e123517e2fa9ad3a0a4c69b8f3e15f3
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-03-15 13:49:32 -0300

    Salário

---
+++ Ex. 1008 URI (Salário)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, h;
+            double v, s;
+
+            n = int.Parse(Console.ReadLine());
+            h = int.Parse(Console.ReadLine());
+
+            v = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);             
+
+            s = h * v;
+
+            Console.WriteLine("NUMBER = " + n);
+            Console.WriteLine("SALARY = U$ " + s.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
