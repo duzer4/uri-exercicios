11f5e3ae4fe08d2a26c414b7364ba08cde3a5847
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-02 12:38:09 -0300

    O Maior

---
+++ Ex. 1013 URI (O Maior)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex._1013_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, maiorab, maior;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            a = int.Parse(valores[0]);
+            b = int.Parse(valores[1]);
+            c = int.Parse(valores[2]);
+
+            maiorab = (a + b + Math.Abs(a - b)) / 2;
+
+            maior = (maiorab + c + Math.Abs(maiorab - c)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
