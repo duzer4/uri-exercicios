c2f747dba9029f545c9b5707283ecda552dc3307
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-02 12:42:33 -0300

    Consumo

---
+++ Ex. 1014 URI (Consumo)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1014_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double y, z;
+
+            x = int.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            z = (double) x / y;
+
+            Console.WriteLine(z.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
