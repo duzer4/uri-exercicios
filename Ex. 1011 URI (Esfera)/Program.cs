95dd63959d47ab1eda31111eee85e3ae31958f66
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-02 12:29:58 -0300

    Esfera

---
+++ Ex. 1011 URI (Esfera)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1011_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, pi, v;            
+
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            pi = 3.14159;
+            v = (double) (4.0 / 3) * pi * Math.Pow(r, 3.0);
+
+            Console.WriteLine("VOLUME = " + v.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
