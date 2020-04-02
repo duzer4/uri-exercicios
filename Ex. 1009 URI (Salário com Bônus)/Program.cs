bd6121dc09178bb9850a475786d071fe2719ea65
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-02 12:24:50 -0300

    Salário com Bônus

---
+++ Ex. 1009 URI (Salário com Bônus)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1009_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, vendas, total;
+
+            nome = (Console.ReadLine());
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            total = (double) vendas * 15 / 100 + salario;
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
