5a6da5bf4ea1f18a31f07b0bc1c05a58876135db
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-14 00:34:53 -0300

    Distância

---
+++ Ex. 1016 URI (Distância)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex._1016_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, t;
+
+            d = int.Parse(Console.ReadLine());
+            t = d * 2;
+            Console.WriteLine(t + " minutos");
+        }
+    }
+}
