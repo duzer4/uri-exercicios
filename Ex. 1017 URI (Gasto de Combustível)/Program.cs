9b031fc27917fa097ec96da0aee393de896c7efc
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-27 19:46:35 -0300

    Gasto de Combustível

---
+++ Ex. 1017 URI (Gasto de Combustível)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1017_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, velocidade, distancia;
+            double litros;
+
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+            distancia = tempo * velocidade;
+
+            litros = (double) distancia / 12;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
