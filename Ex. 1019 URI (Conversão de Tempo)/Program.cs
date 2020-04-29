89e447fa631a9730c9df0083d5e7698849f499eb
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-04-29 20:58:31 -0300

    Conversão de Tempo

---
+++ Ex. 1019 URI (Conversão de Tempo)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex._1019_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, horas, minutos, segundos, resto;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = N % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+                
+        }
+    }
+}
