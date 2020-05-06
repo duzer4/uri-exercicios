4d94bd520dfd617cca94430d01a3444d45b55b2a
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-05-05 23:43:14 -0300

    Fórmula de Bhaskara

---
+++ Ex. 1036 URI (Fórmula de Bhaskara)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1036_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = (double) ((Math.Pow(b, 2.0))) - 4.0 * a * c;
+
+            if (a == 0.0 || delta <= 0.0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                x1 = (double) (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (double) (-b - Math.Sqrt(delta)) / (2 * a);
+
+                Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
