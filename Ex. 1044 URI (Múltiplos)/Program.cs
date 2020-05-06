f3dea8046cd2eacbe797bfc61c9c1989214e516c
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-05-06 20:21:08 -0300

    Múltiplos

---
+++ Ex. 1044 URI (Múltiplos)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex._1044_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+
+            string[] vs = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vs[0]);
+            B = int.Parse(vs[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
