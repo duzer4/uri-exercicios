caafdc20fe6ebe56b7a93867f4746228edbaed8c
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-05-06 08:25:07 -0300

    Lanche

---
+++ Ex. 1038 URI (Lanche)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1038_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+
+            int codigo = int.Parse(vs[0]);
+            int quantidade = int.Parse(vs[1]);
+
+            double total;
+
+            if (codigo == 1)
+            {
+                total = quantidade * 4.0;
+            }
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.5;
+            }
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.0;
+            }
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.0;
+            }
+            else
+            {
+                total = quantidade * 1.5;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
