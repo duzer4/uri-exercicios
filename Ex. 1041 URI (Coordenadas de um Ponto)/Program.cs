ce8dfedbbeb110a97012d33072b4a56c89662565
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-05-06 15:30:48 -0300

    Coordenadas de um Ponto

---
+++ Ex. 1041 URI (Coordenadas de um Ponto)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1041_URI__Coordenadas_de_um_Ponto_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+
+            string[] vs = Console.ReadLine().Split(' ');
+
+            x = double.Parse(vs[0], CultureInfo.InvariantCulture);
+            y = double.Parse(vs[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
