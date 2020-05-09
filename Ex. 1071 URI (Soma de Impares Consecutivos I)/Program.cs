0d108dcf9c4356069753da32f3aea95e5f5b4112
José Eduardo <57038911+duzer4@users.noreply.github.com>
2020-05-09 18:43:06 -0300

    Soma de Ímpares Consecutivos I

---
+++ Ex. 1071 URI (Soma de Impares Consecutivos I)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex._1071_URI__Soma_de_Impares_Consecutivos_I_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, min, max, soma;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+
+            soma = 0;
+            for (int i=min+1; i<max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
