4dab6a1b3681dd295a301105156b8298121922d6
José Eduardo <57038911+eduardo12012001@users.noreply.github.com>
2020-05-08 23:02:54 -0300

    Tempo de Jogo

---
+++ Ex. 1046 URI (Tempo de Jogo)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex._1046_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicial, final, duracao;
+
+            string[] vs = Console.ReadLine().Split(' ');
+
+            inicial = int.Parse(vs[0]);
+            final = int.Parse(vs[1]);
+
+            if (inicial < final)
+            {
+                duracao = final - inicial;
+            }
+            else
+            {
+                duracao = 24 - inicial + final;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+
+        }
+    }
+}
