c0731c4b4b5d0cd9c2c66d7b78ea17835bbfb570
José Eduardo <57038911+duzer4@users.noreply.github.com>
2020-05-13 11:07:39 -0300

    Resto 2

---
+++ Ex. 1075 URI (Resto 2)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex._1075_URI__Resto_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 10000; i++)
+            {
+                if (i % N == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
