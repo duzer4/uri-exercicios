01e24c66f145ec3c6bb02dfae0b0d7dbeae1d55a
José Eduardo <57038911+duzer4@users.noreply.github.com>
2020-05-16 01:14:32 -0300

    Maior e Posição

---
+++ Ex. 1080 URI (Maior e Posição)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex._1080_URI__Maior_e_Posição_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = int.Parse(Console.ReadLine());
+            int posicao = 1;
+
+            for (int i = 2; i <= 100; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x > maior)
+                {
+                    maior = x;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
