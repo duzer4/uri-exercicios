a1f2d7280ebc06eb42a7438570a93303f232ae6b
José Eduardo <57038911+duzer4@users.noreply.github.com>
2020-05-15 23:19:05 -0300

    Tabuada

---
+++ Ex. 1078 URI (Tabuada)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex._1078_URI__Tabuada_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=10; i++)
+            {
+                int resultado = i * N;
+                Console.WriteLine(i + " x " + N + " = " + resultado);
+            }
+        }
+    }
+}
